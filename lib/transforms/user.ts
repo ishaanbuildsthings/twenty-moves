763e1ee96505f79a4904379a24665678bd7c4f4a
Riley Woo <mylifeisriley@gmail.com>
2026-03-24 20:08:27 -0700

    Add get and me routes for user

--- lib/transforms/user.ts
+++ lib/transforms/user.ts
@@ -7,11 +7,26 @@ export interface IUser {
   lastName: string;
 }
 
+export interface IPrivateUser extends IUser {
+  supabaseId: string;
+  wcaId: string | null;
+  createdAt: Date;
+}
+
 export function userToIUser(user: User): IUser {
   return {
     id: user.id,
     username: user.username,
     firstName: user.firstName,
     lastName: user.lastName,
+  };
+}
+
+export function userToIPrivateUser(user: User): IPrivateUser {
+  return {
+    ...userToIUser(user),
+    supabaseId: user.supabaseId,
+    wcaId: user.wcaId,
+    createdAt: user.createdAt,
   };
 }
--- lib/trpc/routers/user.ts
+++ lib/trpc/routers/user.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, authedProcedure } from "../init";
 import { userService } from "@/lib/services/user";
 import { userToIUser } from "@/lib/transforms/user";
@@ -7,4 +8,16 @@ export const userRouter = createTRPCRouter({
     const users = await userService(ctx).list();
     return users.map(userToIUser);
   }),
+
+  me: authedProcedure.query(async ({ ctx }) => {
+    const user = await userService(ctx).getById(ctx.viewer.userId);
+    return userToIUser(user);
+  }),
+
+  get: authedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const user = await userService(ctx).getById(input.id);
+      return userToIUser(user);
+    }),
 });
